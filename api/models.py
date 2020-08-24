c1edff0992f80a0e1819f3c52035c907485d18c7
sali73 <sally.elgendy@hotmail.com>
2020-08-23 21:19:25 -0400

    deploy to heroku

--- api/models.py
+++ api/models.py
@@ -1,6 +1,3 @@
-from django.db import models
-
-# Create your models here.
 # Create your models here.
 from django.db import models
 from django.utils.text import slugify
@@ -12,7 +9,7 @@ class Project(models.Model):
     image = models.CharField(max_length=400)
     slug = models.SlugField(null=True, blank=True)
     def __str__(self):
-        return '%s' % (self.title)
+        return '%s %s %s %s %s ' % (self.backend, self.title, self.frontend, self.deployment, self.image)
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)[:50]
@@ -23,4 +20,4 @@ class Note(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     def __str__(self):
-        return "%s" % (self.title)
+        return "%s %s" % (self.title, self.body)
--- api/resources.py
+++ api/resources.py
@@ -3,11 +3,13 @@ from tastypie.resources import ModelResource, ALL_WITH_RELATIONS
 from tastypie import fields
 from api.models import Project, Note
 from tastypie.authorization import Authorization
+
 class ProjectResource(ModelResource):
     class Meta:
         queryset = Project.objects.all()
         resource_name = 'project'
         authorization = Authorization()
+
 class NoteResource(ModelResource):
     project = fields.ForeignKey(ProjectResource, 'project')
     class Meta:
