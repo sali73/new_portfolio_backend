e07ae15b80fce1e7fa991fde1eecdeda9cfcbb11
sali73 <sally.elgendy@hotmail.com>
2020-08-23 21:06:00 -0400

    deploy to heroku

--- new_portfolio_backend/settings.py
+++ new_portfolio_backend/settings.py
@@ -128,8 +128,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
